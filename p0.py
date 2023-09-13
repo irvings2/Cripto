639db26951a2d9d891f3f41671a663c3b9197a57
irvings2 <88854552+irvings2@users.noreply.github.com>
2023-09-13 00:10:08 -0600

    Cifrado

--- p0.py
+++ p0.py
@@ -4,26 +4,28 @@ with open("cancion.txt") as file_object:
     for i in range(len(leer)):
         if leer[i]!="\n" and leer[i]!=" " and leer[i]!="'" and leer[i]!="," and leer[i]!="?" and leer[i]!="(" and leer[i]!=")" and leer[i]!="-" and leer[i]!="!":
             CadenaLimpia+=leer[i]
-    c=26
+    file_object.close()
+
+with open("cancion.txt") as file_object:
+    leer = file_object.read()
+    c=25
     if c>25:
         print("Ingrese un numero entre 1 y 25")
         exit(1)
     else:
         CadenaFinal=""
         for i in range(len(CadenaLimpia)):
-            if ord(CadenaLimpia[i])==120:
-                 CadenaFinal+=chr(96+c-2)
-            elif ord(CadenaLimpia[i])==121:
-                 CadenaFinal+=chr(96+c-1)
-            elif ord(CadenaLimpia[i])==122:
-                 CadenaFinal+=chr(96+c)
-            else: 
+            if ord(CadenaLimpia[i])+c>122:
+                 aux=122-ord(CadenaLimpia[i])
+                 CadenaFinal+=chr(96+c-aux)
+            else:     
                 CadenaFinal+=chr(ord(CadenaLimpia[i])+c)
+        print(CadenaFinal)
         cifrado=""
         for i in range(len(CadenaFinal)):
             cifrado+=chr(ord(CadenaFinal[i])-32)
+    file_object.close()
 
 with open("cancion_c.txt", "w") as file_object:
         file_object.write(cifrado)
-        
-
+        file_object.close()
